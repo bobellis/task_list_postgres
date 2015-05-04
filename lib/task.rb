58e4415fdd095c98441f171ba15295320f065131
chris sefkin & bob ellis <student@epicodus.com>
2015-05-04 14:11:02 -0700

    final working files

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,10 @@
 class Task
+attr_reader(:description, :list_id)
+# @@task_list = []
 
-@@task_list = []
-
-  define_method(:initialize) do |description|
-    @description = description
+  define_method(:initialize) do |attributes|
+    @description = attributes.fetch(:description)
+    @list_id = attributes.fetch(:list_id)
   end
 
   define_method(:description) do
@@ -11,19 +12,28 @@ class Task
   end
 
   define_singleton_method(:all) do
-    @@task_list
+    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch("description")
+      list_id = task.fetch("list_id").to_i()
+      tasks.push(Task.new({:description => description, :list_id => list_id}))
+    end
+    tasks
   end
+
 
   define_method(:save) do
-    @@task_list.push(self)
+    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
   end
 
-  define_singleton_method(:clear) do
-    @@task_list = []
-  end
+  # define_singleton_method(:clear) do
+  #   @@task_list = []
+  # end
 
   define_method(:==) do |another_task|
-    self.description().==(another_task.description())
+    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
+
   end
 
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,24 +2,33 @@ require('rspec')
 require('task')
 require('pry')
 require('pg')
+require("spec_helper")
 
-DB = PG.connect({:dbname => 'to_do_test'})
-
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM tasks *;")
-  end
-end
+# DB = PG.connect({:dbname => 'to_do_test'})
+#
+# RSpec.configure do |config|
+#   config.after(:each) do
+#     DB.exec("DELETE FROM tasks *;")
+#   end
+# end
 
 describe(Task) do
-  before() do
-    Task.clear()
+  describe("#==") do
+    it("it is the same task if it has the same description") do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(task1).to(eq(task2))
+    end
   end
+
+  # before() do
+  #   Task.clear()
+  # end
 
   describe('#description') do
     it('lets you give it a description') do
-      test_task = Task.new("reticulate the spines")
-      expect(test_task.description()).to(eq("reticulate the spines"))
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.description()).to(eq("learn SQL"))
     end
   end
 
@@ -31,18 +40,29 @@ describe(Task) do
 
   describe('#save') do
     it('adds a task to the array of saved tasks') do
-      test_task = Task.new('reticulate the spine')
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
   end
 
-  describe('.clear') do
-    it('clears all saved tasks') do
-      Task.new('reticluate the spines').save()
-      Task.clear()
-      expect(Task.all()).to(eq([]))
+  describe('#list_id') do
+    it("lets you read the list ID out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
     end
   end
+
+
+
+
+
+  # describe('.clear') do
+  #   it('clears all saved tasks') do
+  #     Task.new({:description => "learn SQL"}).save()
+  #     Task.clear()
+  #     expect(Task.all()).to(eq([]))
+  #   end
+  # end
 
 end
